0b5abe9bfe76a48301fd9cc572f8eda4a7babc5e
Yu Zheng <ypzheng@umass.edu>
2017-04-13 15:00:55 -0400

    'static

--- server/src/server.js
+++ server/src/server.js
@@ -3,30 +3,12 @@ var express = require('express');
 // Creates an Express server.
 var app = express();
 var bodyParser = require('body-parser');
-var util=require('./util.js');
-var reverseString=util.reverseString;
 app.use(bodyParser.text());
+app.use(express.static('../client/build'));
 
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+
 
 // Starts the server on port 3000!
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
-
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  // If the request came with text, then the text() middleware handled it
-  // and made `req.body` a string.
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    // POST did not contain a string. Send an error code back!
-    res.status(400).end();
-  }
 });
